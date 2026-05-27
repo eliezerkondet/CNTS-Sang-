9b0d42735c7b9601f75ac7c1c75d928e288e6934
eliezerkondet <eliezerkondet@gmail.com>
2026-05-27 21:46:19 +0200

    Automatic admin creation superuser

--- create_superuser.py
+++ create_superuser.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 import os
+import sys
 
 User = get_user_model()
 
@@ -7,12 +8,17 @@ username = os.getenv("DJANGO_SUPERUSER_USERNAME")
 email = os.getenv("DJANGO_SUPERUSER_EMAIL")
 password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
 
+# Vérification stricte
+if not username or not password:
+    print("❌ Variables DJANGO_SUPERUSER_USERNAME ou PASSWORD manquantes")
+    sys.exit(1)
+
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(
         username=username,
-        email=email,
+        email=email or "",
         password=password
     )
-    print("Superuser créé avec succès.")
+    print("✅ Superuser créé avec succès.")
 else:
-    print("Le superuser existe déjà.")
+    print("ℹ️ Le superuser existe déjà.")
